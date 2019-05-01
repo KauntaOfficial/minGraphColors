5a49185fa3c50551a50e5d7838f2c01e47282714
ImDaNoob <bjchappell02@icloud.com>
2019-05-01 14:32:02 -0400

    working on processing for kMeans algorithm

---
+++ processGraph.py
@@ -0,0 +1,16 @@
+# Benjamin Chappell
+
+fileToProcess = "DSJC250.1.gr"
+
+def main(f):
+    pFile = open(f, "r+")
+    adjList = pFile.readlines()
+    del adjList[0]
+    
+    graph = []
+
+    for i in range(0, )
+
+
+if __name__ == "__main__":
+    main(fileToProcess)
