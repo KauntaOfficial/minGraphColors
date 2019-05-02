1afd452fbd3e12246fd6fead7075e35d336dca25
Ivy Zhang <ZIvy042003@gmail.com>
2019-05-02 12:54:54 -0400

    Update minGraph.java

--- minGraph.java
+++ minGraph.java
@@ -1,1 +1,51 @@
-// New file
+// Ivy Zhang, Joe, Neo, and Ben
+
+import java.*;
+
+public class minGraph
+{
+    public static int randomInt(int min, int max) 
+    {
+        return rand.nextInt((max - min) + 1) + min;  
+    }
+    
+    public static boolean colorTest(int[] colors, Graph g)
+    {
+        for(int u = 0; u < g.getVertexCount(); u++)
+            for(int v = 0; v < g.getVertexCount(); v++)
+                if(u != v && g.areAdjacent(u, v) && colors[u] == colors[v])
+                {
+                    System.out.println("Vertices " + u + " and " + v + " are adjacent and have color " + colors[u] + ".");
+                    return false;
+                }
+        
+        for(int color : colors)
+            System.out.print(color);
+            
+        System.out.println();
+            
+        return true;
+    }
+    
+    public static void main(String[] args)
+    {
+        Graph inputGraph = new Graph();
+        int vertexCount = inputGraph.getVertexCount();
+        int[] vertexColors = new int[vertexCount];
+        
+        for (int colorChoices = vertexCount - 1; colorChoices > 0; colorChoices--)
+        {
+            for (int attempt = 0; attempt <= 100; attempt++)
+            {
+                for (int node = 0; node < vertexColors.length; node++)
+                {
+                    vertexColors[node] = randomInt(0, colorChoices);
+                }
+                
+                if (colorTest(vertexColors, inputGraph))
+                    break;
+                
+            }
+        }
+    }
+}
