2df9e42ad4f1a9d7e1181b224e228063a0fdd888
ImDaNoob <bjchappell02@icloud.com>
2019-05-02 10:39:16 -0400

    finished, need to fix the original graph file

--- processGraph.py
+++ processGraph.py
@@ -1,16 +1,33 @@
 # Benjamin Chappell
 
 fileToProcess = "DSJC250.1.gr"
+fileToReturn = "dsjc2501.txt"
 
-def main(f):
-    pFile = open(f, "r+")
+def main(f, w):
+    pFile = open(f, "r")
     adjList = pFile.readlines()
-    del adjList[0]
+    vertexCount = int(adjList.pop(0))
+
+    wFile = open(w, "w")
+
+    for i in range(0, len(adjList)):
+        adjList[i] = adjList[i].split()
+        
+        for j in range(0, len(adjList[i])):
+            adjList[i][j] = int(adjList[i][j])
+
+        del adjList[i][0]
+
+    adjMatrix = [[0 for i in range(0, vertexCount)] for i in range(0, vertexCount)]
     
-    graph = []
+    for i in range(0, vertexCount):
+        for j in range(0, len(adjList[i])):
+            adjMatrix[i][adjList[i][j]] = 1
 
-    for i in range(0, )
-
+    for i in adjMatrix:
+        for j in i:
+            wFile.write(str(j) + " ")
+        wFile.write("\n")
 
 if __name__ == "__main__":
-    main(fileToProcess)
+    main(fileToProcess, fileToReturn)
