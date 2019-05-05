afe5f57470200f84cb967dbce9e15292c73fd4ab
ImDaNoob <bjchappell02@icloud.com>
2019-05-05 16:57:44 -0400

    fixed the 2d surface adj list

--- processGraph.py
+++ processGraph.py
@@ -16,14 +16,14 @@ def main(f, w):
 
         del adjList[i][0]
 
-    print (adjList)
+    #print (adjList)
 
     adjMatrix = [[0 for i in range(0, vertexCount)] for i in range(0, vertexCount)]
     
     for i in range(0, len(adjList)):
-        #print (i)
         for j in range(0, len(adjList[i])):
             adjMatrix[i][adjList[i][j]] = 1
+            print (str(i) + " " + str(j))
 
     for i in adjMatrix:
         for j in i:
