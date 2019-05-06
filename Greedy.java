3f4025fa0f8f6705985932ab3db0c0d99fb01e65
ImDaNoob <bjchappell02@icloud.com>
2019-05-06 12:39:55 -0400

    working on greedy

---
+++ Greedy.java
@@ -0,0 +1,12 @@
+// Benjamin Chappell
+
+public class Greedy
+{
+    public static void main(String[] args)
+    {
+        String currentFile = "DSJC125.1.gr"
+        Graph currentGraph = new Graph(currentFile)
+
+        
+    }
+}
