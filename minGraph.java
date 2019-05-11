4c54b80bff55dfe06f2b1f91976e8e280b0cd3d8
jseaton <jseaton612@gmail.com>
2019-05-10 20:18:43 -0400

    make minGraph work again

--- minGraph.java
+++ minGraph.java
@@ -18,36 +18,36 @@ public class minGraph
         return true;
     }
     
-    public static int randomInt(int min, int max) 
+    public static int randomInt(int min, int max)
     {
         int range = (max - min) + 1;
-        return (int)(Math.random() * range) + min;  
+        return (int)(Math.random() * range) + min;
     }
     
-    static int countDistinct(int arr[], int n) 
-    { 
-        int res = 1; 
+    static int countDistinct(int arr[], int n)
+    {
+        int res = 1;
   
-        // Pick all elements one by one 
-        for (int i = 1; i < n; i++)  
-        { 
-            int j = 0; 
+        // Pick all elements one by one
+        for (int i = 1; i < n; i++)
+        {
+            int j = 0;
             
-            for (j = 0; j < i; j++) 
-                if (arr[i] == arr[j]) 
-                    break; 
+            for (j = 0; j < i; j++)
+                if (arr[i] == arr[j])
+                    break;
 
-            // If not printed earlier,  
-            // then print it 
-            if (i == j) 
-                res++; 
-        } 
+            // If not printed earlier,
+            // then print it
+            if (i == j)
+                res++;
+        }
         
-        return res; 
-    } 
+        return res;
+    }
     
     public static void main(String[] args)
-    {        
+    {
         //Create a new graph to manipulate.
         Graph inputGraph = new Graph();
         
@@ -55,11 +55,11 @@ public class minGraph
         int vertexCount = inputGraph.getVertexCount();
         
         //Create an array to store the best vertex color combination.
-        int[] optimumVertexColors = new int[vertexCount];            
-                    
-        for (int timesToRun = 0; timesToRun < 10; timesToRun++)
-        {   
-            long start = System.nanoTime() / 1000000;     
+        int[] optimumVertexColors = new int[vertexCount];
+        
+        for (int timesToRun = 1; timesToRun <= 10; timesToRun++)
+        {
+            long start = System.nanoTime() / 1000000;
 
             /* Random thingy ma bob runthroughs */
             for (int runNumber = 0; runNumber < timesToRun; runNumber++)
@@ -101,10 +101,10 @@ public class minGraph
                         while (adjacentVertexColors.contains(vertexColors[selectedElement]))
                         {
                               vertexColors[selectedElement] = vertexColors[selectedElement] + 1;
-                        } 
+                        }
                     }
 
-                    if (countDistinct(vertexColors, vertexColors.length) < countDistinct(optimumVertexColors, optimumVertexColors.length))
+                    if (node == 0 || countDistinct(vertexColors, vertexColors.length) < countDistinct(optimumVertexColors, optimumVertexColors.length))
                     {
                         //This array copy is good for large datasets.
                         System.arraycopy(vertexColors, 0, optimumVertexColors, 0, vertexColors.length);
@@ -115,7 +115,7 @@ public class minGraph
             long end = System.nanoTime() / 1000000;
 
             //Print the number of times the random algorithm was run, the runtime, and the number of colors
-            System.out.println((timesToRun + 1) + "," + (end - start) + "," + countDistinct(optimumVertexColors, optimumVertexColors.length));
-        }                    
+            System.out.println((timesToRun) + "," + (end - start) + "," + countDistinct(optimumVertexColors, optimumVertexColors.length));
+        }
     }
 }
