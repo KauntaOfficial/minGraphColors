140133df5560facdeaf932b7801191d59b9a4291
ImDaNoob <bjchappell02@icloud.com>
2019-05-14 20:09:37 -0400

    initCentroids compiles, deleted paragraphs from octave version

--- kMeans.java
+++ kMeans.java
@@ -1,28 +1,72 @@
 // By Ivy Zhang and Co.
 
+import java.lang.Math;
 import org.jblas.*;
 
 // Need to make sure that the jblas package is installed.
+// Compile with statement javac -cp '.:jblas-1.2.4.jar' kMeans.java
 
 public class kMeans
 {
     public static int randomInt(int min, int max)
     {
         //int range = (max  min) + 1;
-        return (int)(Math.random() * range) + min;
+        return (int)(Math.random() * max) + min;
     }
     
     public static DoubleMatrix initCentroids(DoubleMatrix X, int K)
     {
-        int randidx = randomInt(0, adjacencyMatrix.length);
+        // Get a random row to start as the first centroid.
+        int randidx = (int)randomInt(0, X.rows);
+        DoubleMatrix centroids = new DoubleMatrix(K, X.columns);
+
+        // Initialize the very first row of the centroids.
+        centroids.putRow(0, X.getRow(randidx));
+        //Start this 0 for convenience's sake.
+        int centroidsComputed = 0;
+
+        while (centroidsComputed < K)
+        {
+            DoubleMatrix weights = DoubleMatrix.zeros(X.columns, 1);
+
+            for (int i = 0; i < X.columns; i++)
+            {
+                // Compute the distance between the row of X and each of the centroids.
+                DoubleMatrix z = centroids.subRowVector(X.getRow(i));
+                
+                DoubleMatrix zy = DoubleMatrix.zeros(K);
+                // Iterate through each of the rows of z. Sums the element-wise distances from 
+                // each centroid, computing overall distance.
+                for (int k = 0; k < K; k++)
+                {
+                    // Iterate through the columns.
+                    for (int j = 0; j < z.columns; j++)
+                    {
+                        zy.put(k, zy.get(k) + Math.pow(z.get(k, j), 2));
+                    }
+                }
+
+                // Choose the weight to be the distance from the centroid closest to the
+                // Current vertex
+                weights.put(i, zy.min());
+            }
+
+            // Choose the vertex furthest away from all the current centroids and add
+            // it as the newest centroid.
+            double maxDist = weights.max();
+            centroidsComputed++;
+            centroids.putRow(centroidsComputed, X.getRow((int)maxDist));
+            System.out.println("Centroid Calculated " + (centroidsComputed + 1));
+        }
         
+        return centroids;
     }
 
     public static DoubleMatrix formAdjMatrix(boolean[][] adjMatrix)
     {   
         // Initialize the length and the new adj matrix.
         int len = adjMatrix.length;
-        DoubleMatrix newAdjMatrix = new DoubleMatrix.zeros(len, len);
+        DoubleMatrix newAdjMatrix = DoubleMatrix.zeros(len, len);
 
         // Create the new adjacency matrix from ones and zeros.
         for (int i = 0; i < len; i++)
@@ -42,11 +86,9 @@ public class kMeans
         Graph inputGraph = new Graph();
         boolean[][] adjMatrix = inputGraph.adjacencyMatrix;
         int numberOfVertices = inputGraph.getVertexCount();
-        int K = Math.sqrt(numberOfVertices);
+        int K = (int)Math.sqrt(numberOfVertices);
         
         DoubleMatrix X = formAdjMatrix(adjMatrix);
-        DoubleMatrix centroids = initCentroids();
-        
-        initCentroids();
+        DoubleMatrix centroids = initCentroids(X, K);
     }
 }
--- kMeansInitCentroids.m
+++ kMeansInitCentroids.m
@@ -25,57 +25,10 @@ while (centroidsComputed < K)
 
   for i = 1:size(X,1)
     % z = centroids .- X(i, :); // Only Octave compatible
-    z = bsxfun(@minus, centroids, X(i, :));
+    z = bsxfun(@minus, centroids, X(i, :))
     zy = sum(z.^2, 2);
     [~, weights(i)] = min(zy);
   end 
- 
-  %%// Spread out centroids to the third dimension so that the singleton
-  %%// second dimension thus created could be used with bsxfun for expansion in
-  %%// that dimension
-  %centroids1 = permute(centroids,[3 2 1]);
-  
-  %%// Perform the much-needed subtraction 
-  %t1 = bsxfun(@minus, X, centroids)
-
-  %%// Perform element-wise squaring and then min-finding as required too
- % t2 = t1.^2
-  %t3 = sum(t2,2)
-
-  %%// Since the expansion resulted in data in third dimension, min-finding
-  %%// must be along it
-  %[mini_array, weights] = min(t3,[],3)
-  
-  % Compute the weights based on the distances from the nearest vertex
-  %for ex=1:size(X,1)
-  %  distances = zeros(1, K);
-
-  %  for k=1:centroidsComputed
-  %    distances(k) = (sum((X(ex,:) - centroids(k,:)).^2));
-  %  end%
-  
-  %  [m weights(ex)] = min(distances);
-  %end
-  
-  %weights = weights .^ 3;
-  
-  % Choose a random, weighted node based on the distances from the centroids.
-  % Sum the weights of the nodes.
-  %sumOfWeights = sum(weights);
-  
-  % Get a random number between 1 and the sum of the weights.
-  %rnd = randi(sumOfWeights);
-  
-  % Iterate through the list of items, subtracting the weight from the random number 
-  % Until a weight is greater than the remaining numbers.
-  %for i=1:size(X,1)
-  %  if rnd < weights(i)
-  %    centroidsComputed = centroidsComputed + 1;
-  %    centroids(centroidsComputed, :) = X(i, :);
-  %    break
-  %  end
-  %  rnd = rnd - weights(i);
-  %end
   
   % Choose the vertex furthest away from all current centroids and add it
   % as the newest centroid.
